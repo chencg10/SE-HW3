109cc90ea346052d8601a1f3537542c31f6c50a4
chencg10 <ccoheng@gmail.com>
2023-06-27 22:17:38 +0300

    Update BarberShopGUI.java

--- SE-HW3/src/BarberShop/BarberShopGUI.java
+++ SE-HW3/src/BarberShop/BarberShopGUI.java
@@ -6,6 +6,11 @@ public class BarberShopGUI {
     //members:
     private BarberShop _barberShop;
     private JFrame _frame;
+    private JPanel _panel;
+    private JButton _cashRegisterButton;
+    private JButton _CustomersMenuButton;
+    private JButton _ServicesMenuButton;
+    private JButton _CalendarMenuButton;
 
 
     //constructor:
@@ -13,26 +18,10 @@ public class BarberShopGUI {
 
         this._barberShop = barberShop;
         // Create the frame
-        this._frame = new JFrame("Barber Shop");
-        // Set the frame's default close operation to exit on close
-        this._frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // Set the frame's size to 1000x667
-        this._frame.setSize(1000, 667);
-        // Set the frame's layout to border layout
-        this._frame.setLayout(new BorderLayout());
-
-        // Set the frame to be not resizable
-        this._frame.setResizable(false);
-
-        // Create a background image
-//        ImageIcon background = new ImageIcon("src/BarberShop/BarberBackround.jpeg");
-//        // Create a label for the background image
-//        JLabel backgroundLabel = new JLabel(background);
-//        backgroundLabel.setBounds(0, 0, background.getIconWidth(), background.getIconHeight());
-//        this._frame.add(backgroundLabel, BorderLayout.CENTER);
+        this.setFrame();
 
         // Create a JPanel with a custom background color or image
-        JPanel panel = new JPanel() {
+        this._panel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
@@ -42,7 +31,11 @@ public class BarberShopGUI {
             }
         };
 
-        panel.setLayout(new FlowLayout());
+        this._panel.setLayout(new FlowLayout());
+
+        // Create the buttons
+        this.setCashRegisterButton();
+        this.setCustomersMenuButton();
 
         // Set the title of the panel
         JLabel titleLabel = new JLabel("Barber Shop");
@@ -53,16 +46,84 @@ public class BarberShopGUI {
         titleLabel.setVerticalAlignment(JLabel.CENTER);
         titleLabel.setOpaque(false);
         //this._frame.add(titleLabel, BorderLayout.NORTH);
-        panel.add(titleLabel);
-        this._frame.getContentPane().add(panel);
+        this._panel.add(titleLabel);
+        // add buttons to the panel
+        this._panel.add(this._cashRegisterButton, BorderLayout.EAST);
+        this._panel.add(this._CustomersMenuButton, BorderLayout.EAST);
+
+        this._frame.getContentPane().add(this._panel);
 
         // Make the frame visible
         this._frame.setVisible(true);
     }
 
+    private void setCustomersMenuButton() {
+        this._CustomersMenuButton = new JButton("Customers Menu");
+        this._CustomersMenuButton.setFont(new Font("MV Boli", Font.PLAIN, 16));
+        this._CustomersMenuButton.setForeground(Color.white);
+        this._CustomersMenuButton.setBackground(Color.white);
+        this._CustomersMenuButton.setOpaque(false);
+        this._CustomersMenuButton.setBorderPainted(false);
+        // set button position
+        this._CustomersMenuButton.setBounds(0, 0, 50, 25);
+        // set the button's listener
+        this._CustomersMenuButton.addActionListener(e -> {
+            this._frame.getContentPane().removeAll();
+            this._frame.getContentPane().repaint();
+            this._frame.getContentPane().revalidate();
+            this._frame.getContentPane().add(this._panel);
+            this._frame.getContentPane().repaint();
+            this._frame.getContentPane().revalidate();
+
+            //add functionallity
+            System.out.println(this._barberShop.getCustomersList());
+        });
+        this._panel.add(this._CustomersMenuButton);
+    }
+
+    private void setCashRegisterButton() {
+        this._cashRegisterButton = new JButton("Cash Register");
+        this._cashRegisterButton.setFont(new Font("MV Boli", Font.PLAIN, 16));
+        this._cashRegisterButton.setForeground(Color.white);
+        this._cashRegisterButton.setBackground(Color.white);
+        this._cashRegisterButton.setOpaque(false);
+        this._cashRegisterButton.setBorderPainted(false);
+        // set button position
+        this._cashRegisterButton.setBounds(0, 25, 50, 25);
+        // set the button's listener
+        this._cashRegisterButton.addActionListener(e -> {
+            this._frame.getContentPane().removeAll();
+            this._frame.getContentPane().repaint();
+            this._frame.getContentPane().revalidate();
+            this._frame.getContentPane().add(this._panel);
+            this._frame.getContentPane().repaint();
+            this._frame.getContentPane().revalidate();
+
+            //add functionallity
+            System.out.println(this._barberShop.getCashRegister());
+        });
+        this._panel.add(this._cashRegisterButton);
+    }
+
+
+    private void setFrame()
+    {
+        this._frame = new JFrame("Barber Shop");
+        // Set the frame's default close operation to exit on close
+        this._frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Set the frame's size to 1000x667
+        this._frame.setSize(1000, 667);
+        // Set the frame's layout to border layout
+        this._frame.setLayout(new BorderLayout());
+
+        // Set the frame to be not resizable
+        this._frame.setResizable(false);
+    }
+
     //main function:
     public static void main(String[] args) {
         BarberShop barberShop = new BarberShop();
+
         BarberShopGUI gui = new BarberShopGUI(barberShop);
     }
 
